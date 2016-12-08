f6737775421387dbea0a4cb4c6e5e041a581b8b9
carinecroteau <croteau.carine@carrefour.cegepvicto.ca>
2016-12-08 12:46:36 -0500

    Correction

--- ExamenFormatif3/ExamenFormatif3/Program2.cs
+++ ExamenFormatif3/ExamenFormatif3/Program2.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 //Eric Lacroix
 //29-11-2016
 //ExamenFormatif3 Question 5 Programme déplacement personnage
+// 10/15
+// 
 
 namespace ExamenFormatif3
 {
@@ -27,7 +29,7 @@ namespace ExamenFormatif3
             int cptFalse = 0;
             //Création du tableau
            
-            for (int i = 0; i < tabDeplacement.Length; i++)
+            for (int i = 0; i < tabDeplacement.Length; i++)             // CC : Super!
             {
                 nombreAleatoire = rnd.Next(0, 2);
                 if (nombreAleatoire == 1)
@@ -43,6 +45,8 @@ namespace ExamenFormatif3
                
 
             }
+
+            // CC : Ne pas afficher le tableau dès le départ, seulement sur la touche Y
             AfficheEntier(tabDeplacement);
             Console.WriteLine("\n" + "position de Départ de BOB : " + positionPersonX);
             touche = Console.ReadKey(true);
@@ -54,9 +58,10 @@ namespace ExamenFormatif3
                 {
 
                     case ConsoleKey.A:
+                        // CC : Il faut vérifier si le déplacement est possible avant de changer la position du personnage. 
                         positionPersonX -= 3;
                         Console.Write("\n" + "position de BOB : " + positionPersonX + " " + tabDeplacement[positionPersonX] + "\n");
-                        positionPersonX++;
+                        positionPersonX++;      // CC : Le personnage avance d'une autre case?
                         for (int i = 0; i < 4; i++)
                         {
                             positionPersonX++;
@@ -146,6 +151,8 @@ namespace ExamenFormatif3
                     case ConsoleKey.P:
                         Affichage10(tabDeplacement, positionPersonX);
                         break;
+
+                    // case Y?
                 }
                 cptessais++;
             } 
@@ -158,6 +165,8 @@ namespace ExamenFormatif3
         }
 
 
+
+        // Commenter la fonction
         static void AfficheEntier(bool[] tabDeplacement)
         {
 
@@ -166,7 +175,10 @@ namespace ExamenFormatif3
                 Console.Write(" " + i + " " + tabDeplacement[i] + " ");
             }
         }
-            static void Affichage10(bool[] tabDeplacement,int positionPersonX)
+
+
+        // Commenter la fonction et espacer les deux fonctions (au moins une ligne blanche entre les deux fonctions)
+        static void Affichage10(bool[] tabDeplacement,int positionPersonX)
             {
             for (int i = positionPersonX + 1; i < positionPersonX + 11; i++)
              {
